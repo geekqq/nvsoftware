5c5711c1f75e8794edf34eb7c599e42db2c423f9
geekqq <jinwei1014@gmail.com>
2023-05-05 15:12:56 -0700

    add order service

---
+++ iOrderService/src/test/java/net/nvsoftware/iorderservice/IOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.iorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
