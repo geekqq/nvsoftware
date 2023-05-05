00ec2514ba95f9b1b4cf3cddf5f264af533127cd
geekqq <jinwei1014@gmail.com>
2023-05-05 12:57:55 -0700

    git initial service registry

---
+++ iServiceRegistry/src/test/java/net/nvsoftware/iserviceregistry/IServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.iserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
