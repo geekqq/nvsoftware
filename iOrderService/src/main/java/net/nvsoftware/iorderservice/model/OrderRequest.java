1eb04a886642d9fb511d1690caad49620018b988
geekqq <jinwei1014@gmail.com>
2023-05-20 22:26:35 -0700

    place order tested

--- iOrderService/src/main/java/net/nvsoftware/iorderservice/model/OrderRequest.java
+++ iOrderService/src/main/java/net/nvsoftware/iorderservice/model/OrderRequest.java
@@ -1,11 +1,17 @@
 package net.nvsoftware.iorderservice.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderRequest {
     private long productId;
     private long orderQuantity;
--- iOrderService/src/test/java/net/nvsoftware/iorderservice/service/OrderServiceImplTest.java
+++ iOrderService/src/test/java/net/nvsoftware/iorderservice/service/OrderServiceImplTest.java
@@ -3,8 +3,10 @@ package net.nvsoftware.iorderservice.service;
 import net.nvsoftware.iorderservice.entity.OrderEntity;
 import net.nvsoftware.iorderservice.external.client.PaymentServiceFeignClient;
 import net.nvsoftware.iorderservice.external.client.ProductServiceFeignClient;
+import net.nvsoftware.iorderservice.model.OrderRequest;
 import net.nvsoftware.iorderservice.model.OrderResponse;
 import net.nvsoftware.iorderservice.model.PaymentMode;
+import net.nvsoftware.iorderservice.model.PaymentRequest;
 import net.nvsoftware.iorderservice.repository.OrderRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -13,10 +15,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Instant;
 import java.util.Optional;
+
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class OrderServiceImplTest {
@@ -39,14 +45,14 @@ class OrderServiceImplTest {
     void testWhenGetOrderSuccess() {
         //Mock Part
         OrderEntity orderEntity = getMockOrderEntity();
-        Mockito.when(orderRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(orderEntity));
+        when(orderRepository.findById(anyLong())).thenReturn(Optional.of(orderEntity));
 
-        Mockito.when(restTemplate.getForObject(
+        when(restTemplate.getForObject(
                 "http://PRODUCT-SERVICE/products/" + orderEntity.getProductId(),
                 OrderResponse.ProductResponse.class
         )).thenReturn(getMockProductResponse());
 
-        Mockito.when(restTemplate.getForObject(
+        when(restTemplate.getForObject(
                 "http://PAYMENT-SERVICE/payments/" + orderEntity.getOrderId(),
                 OrderResponse.PaymentResponse.class
         )).thenReturn(getMockPaymentResponse());
@@ -55,12 +61,12 @@ class OrderServiceImplTest {
         OrderResponse orderResponse = orderService.getOrderDetailByOrderId(1);
 
         //Verify call
-        Mockito.verify(orderRepository, Mockito.times(1)).findById(Mockito.anyLong());
-        Mockito.verify(restTemplate, Mockito.times(1)).getForObject(
+        verify(orderRepository, times(1)).findById(anyLong());
+        verify(restTemplate, times(1)).getForObject(
                 "http://PRODUCT-SERVICE/products/" + orderEntity.getProductId(),
                 OrderResponse.ProductResponse.class
         );
-        Mockito.verify(restTemplate, Mockito.times(1)).getForObject(
+        verify(restTemplate, times(1)).getForObject(
                 "http://PAYMENT_SERVICE/payments/" + orderEntity.getOrderId(),
                 OrderResponse.PaymentResponse.class
         );
@@ -68,6 +74,75 @@ class OrderServiceImplTest {
         //Assert response
         Assertions.assertNotNull(orderResponse);
         Assertions.assertEquals(orderEntity.getOrderId(), orderResponse.getOrderId());
+    }
+
+
+    @Test
+    @DisplayName("Get order detail OrderId Not Found - FAILED")
+    void testWhenOrderIdNotFound() {
+        when(orderRepository.findById(anyLong())).thenReturn(Optional.ofNullable(null));
+
+        RuntimeException runtimeException = Assertions.assertThrows(RuntimeException.class, () -> orderService.getOrderDetailByOrderId(1));
+        Assertions.assertEquals("OrderService getOrderDetailByOrderId NOT FOUND for: 1", runtimeException.getMessage());
+        verify(orderRepository, times(1)).findById(anyLong());
+
+    }
+
+    @Test
+    @DisplayName("Place order - Success")
+    void testWhenPlaceOrderSuccess() {
+
+        OrderEntity orderEntity = getMockOrderEntity();
+        OrderRequest orderRequest = getMockOrderRequest();
+
+        when(orderRepository.save(any(OrderEntity.class)))
+                .thenReturn(orderEntity);
+
+        when(productServiceFeignClient.reduceQuantity(anyLong(),anyLong()))
+                .thenReturn(new ResponseEntity<Void>(HttpStatus.OK));
+        when(paymentServiceFeignClient.doPayment(any(PaymentRequest.class)))
+                .thenReturn(new ResponseEntity<Long>(1L, HttpStatus.OK));
+
+        long orderId = orderService.placeOrder(orderRequest);
+
+        verify(orderRepository, times(2)).save(any());
+        verify(productServiceFeignClient, times(1)).reduceQuantity(anyLong(), anyLong());
+        verify(paymentServiceFeignClient, times(1)).doPayment(any(PaymentRequest.class));
+
+        Assertions.assertEquals(orderEntity.getOrderId(), orderId);
+    }
+
+    @Test
+    @DisplayName("Place order payment failed - Failed")
+    void testWhenPlaceOrderFailed() {
+
+        OrderEntity orderEntity = getMockOrderEntity();
+        OrderRequest orderRequest = getMockOrderRequest();
+
+        when(orderRepository.save(any(OrderEntity.class)))
+                .thenReturn(orderEntity);
+
+        when(productServiceFeignClient.reduceQuantity(anyLong(),anyLong()))
+                .thenReturn(new ResponseEntity<Void>(HttpStatus.OK));
+        when(paymentServiceFeignClient.doPayment(any(PaymentRequest.class)))
+                .thenThrow(new RuntimeException("Payment Failed"));
+
+        long orderId = orderService.placeOrder(orderRequest);
+
+        verify(orderRepository, times(2)).save(any());
+        verify(productServiceFeignClient, times(1)).reduceQuantity(anyLong(), anyLong());
+        verify(paymentServiceFeignClient, times(1)).doPayment(any(PaymentRequest.class));
+
+        Assertions.assertEquals(orderEntity.getOrderId(), orderId);
+    }
+
+    private OrderRequest getMockOrderRequest() {
+        return OrderRequest.builder()
+                .productId(1)
+                .orderQuantity(1)
+                .totalAmount(1299)
+                .paymentMode(PaymentMode.CASH)
+                .build();
     }
 
     private OrderEntity getMockOrderEntity() {
